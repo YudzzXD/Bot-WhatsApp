2ef62218afaaf5ce97b48bee8b8b79681fbb0f13
YudzzXD <choijun832@gmail.com>
2023-03-05 18:59:14 +0700

    YudsBotz

---
+++ options/help.js
@@ -0,0 +1,51 @@
+exports.listmenu = (prefix) => {
+return` 𝗠𝗔𝗜𝗡 𝗠𝗘𝗡𝗨
+ ● ${prefix}owner
+ ● ${prefix}toimg
+ ● ${prefix}sticker
+ ● ${prefix}spamcall
+ ● ${prefix}jadibot
+ ● ${prefix}listjadibot
+ ● ${prefix}infoupdate
+ 
+ 𝗔𝗡𝗢𝗡𝗬𝗠𝗢𝗨𝗦 𝗖𝗛𝗔𝗧
+ ● ${prefix}chat
+ ● ${prefix}skip
+ ● ${prefix}start
+ ● ${prefix}secret
+ ● ${prefix}confess
+ ● ${prefix}menfess
+ ● ${prefix}secretchat
+ ● ${prefix}stopchat
+
+ 𝗚𝗥𝗢𝗨𝗣 𝗠𝗘𝗡𝗨
+ ● ${prefix}hidetag
+ ● ${prefix}tagall
+ ● ${prefix}fitnah
+ ● ${prefix}delete
+ ● ${prefix}revoke
+ ● ${prefix}linkgrup
+ ● ${prefix}setdesc
+ ● ${prefix}demote
+ ● ${prefix}antilink
+ ● ${prefix}promote
+ ● ${prefix}setppgrup
+ ● ${prefix}kick @tag
+ ● ${prefix}setnamegc
+ ● ${prefix}group open
+ ● ${prefix}group close
+
+ 𝗦𝗧𝗔𝗟𝗞𝗘𝗥 𝗠𝗘𝗡𝗨
+ ● ${prefix}ffstalk *id*
+ ● ${prefix}mlstalk *id|zone*
+ ● ${prefix}npmstalk *packname*
+ ● ${prefix}githubstalk *username*
+
+ 𝗦𝗧𝗢𝗥𝗘 𝗠𝗘𝗡𝗨
+ ● ${prefix}list *<only grup>*
+ ● ${prefix}addlist *key@pesan*
+ ● ${prefix}dellist *<options>*
+ ● ${prefix}update *key@pesan*
+ ● ${prefix}proses *<reply orderan>*
+ ● ${prefix}done *<reply orderan>*`
+}
